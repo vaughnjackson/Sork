722c077ea7ef406a03c032d5949f49a211b32c30
Vaughn Jackson <vaughn.jackson@improving.com>
2025-01-02 13:39:31 -0600

    Our first commands

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        do
+        {
+            Console.Write(" > ");
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            input = input.Trim();
+            if (input == "lol") { Console.WriteLine("You laugh out loud!"); }
+            else if (input == "exit") { break; }
+            else { Console.WriteLine("Unknown command"); }
+        } while (true);
+    }
+}
