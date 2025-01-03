a4b4a1467b3db4c9c165d84afef7a227ef709674
Vaughn Jackson <vaughn.jackson@improving.com>
2025-01-03 10:53:14 -0600

    Completed first homework

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@
             input = input.Trim();
             if (input == "lol") { Console.WriteLine("You laugh out loud!"); }
             else if (input == "exit") { break; }
+            else if (input == "dance") { Console.WriteLine("Get down and boogie!"); }
+            else if (input == "sing") { Console.WriteLine("If I was twice the man I could be, I'd be half of what you need!"); }
+            else if (input == "whistle") { Console.WriteLine("Whistle while you work!"); }
             else { Console.WriteLine("Unknown command"); }
         } while (true);
     }
